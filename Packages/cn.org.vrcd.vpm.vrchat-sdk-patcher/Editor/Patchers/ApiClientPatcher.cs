1997ce944382df673b5e5089750bdc0bcd28391e
Misaka-L <lipww1234@foxmail.com>
2024-06-21 23:53:55 +0800

    feature: patch VRC.Core.API proxy

---
+++ Packages/cn.org.vrcd.vpm.vrchat-sdk-patcher/Editor/Patchers/ApiClientPatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BestHTTP;
+using HarmonyLib;
+using VRC;
+using VRC.Core;
+
+namespace VRCD.VRChatPackages.VRChatSDKPatcher.Editor.Patchers
+{
+    [HarmonyPatch(typeof(API), nameof(API.PopulateHTTPRequestHeaders))]
+    internal class ApiClientPatcher
+    {
+        private static void Postfix(HTTPRequest request)
+        {
+            request.RemoveHeader("X-MacAddress");
+
+            request.Proxy = new HTTPProxy(new Uri(PatcherMain.PatcherSettings.HttpProxyUri));
+        }
+    }
+}
