24656da0f656f4815703e830c3324344c976e31f
Misaka-L <lipww1234@foxmail.com>
2025-04-04 00:47:03 +0800

    feat: skip copyright agreement patch

---
+++ Packages/cn.org.vrcd.vpm.vrchat-sdk-patcher/Editor/Patchers/VRCCopyrightAgreementPatcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VRCD.VRChatPackages.VRChatSDKPatcher.Editor.Patchers
+{
+    internal class VRCCopyrightAgreementPatcher : IPatcher
+    {
+        private const string CopyrightAgreementTypeName = "VRC.SDKBase.VRCCopyrightAgreement";
+
+        private static bool Prefix(ref Task<bool> __result)
+        {
+            __result = Task.FromResult(true);
+
+            Debug.LogWarning(
+                "By using this patch, you agree that you have already read and accepted the VRChat copyright agreement.");
+
+            return false;
+        }
+
+        public void Patch(Harmony harmony)
+        {
+            var copyrightAgreementType = AccessTools.TypeByName(CopyrightAgreementTypeName);
+
+            if (copyrightAgreementType == null)
+            {
+                Debug.LogWarning(
+                    "Failed to find VRCCopyrightAgreement type, it may because you are using a old SDK or SDK changed");
+                return;
+            }
+
+            var hasAgreementMethod = AccessTools.Method(copyrightAgreementType, "HasAgreement");
+
+            if (hasAgreementMethod == null)
+            {
+                Debug.LogWarning(
+                    "Failed to find HasAgreement method");
+                return;
+            }
+
+            var patchMethod = AccessTools.Method(typeof(VRCCopyrightAgreementPatcher), nameof(Prefix));
+
+            harmony.Patch(hasAgreementMethod, new HarmonyMethod(patchMethod));
+        }
+    }
+}
