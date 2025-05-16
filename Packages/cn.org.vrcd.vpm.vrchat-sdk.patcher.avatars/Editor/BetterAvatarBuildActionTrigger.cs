f82bbf1d1ce01e93a8a6143a9ef4bb4191f0e471
Misaka-L <lipww1234@foxmail.com>
2025-05-17 02:49:48 +0800

    refactor: adjust priority of build actions menu items

--- Packages/cn.org.vrcd.vpm.vrchat-sdk.patcher.avatars/Editor/BetterAvatarBuildActionTrigger.cs
+++ Packages/cn.org.vrcd.vpm.vrchat-sdk.patcher.avatars/Editor/BetterAvatarBuildActionTrigger.cs
@@ -5,25 +5,25 @@ namespace VRCD.VRChatPackages.VRChatSDKPatcher.Avatars.Editor
 {
     internal static class BetterAvatarBuildActionTrigger
     {
-        [MenuItem("VRChat SDK Patcher/[Avatars] Build Actions/[Build] Only", priority = 1000)]
+        [MenuItem("VRChat SDK Patcher/[Avatars] Build Actions/[Build] Only", priority = 2000)]
         private static void Build()
         {
             AvatarBuildActionUtils.BuildGuard();
         }
 
-        [MenuItem("VRChat SDK Patcher/[Avatars] Build Actions/[Build] and [Test]", priority = 2000)]
+        [MenuItem("VRChat SDK Patcher/[Avatars] Build Actions/[Build] and [Test]", priority = 3000)]
         private static void BuildAndTest()
         {
             AvatarBuildActionUtils.BuildAndTestGuard();
         }
 
-        [MenuItem("VRChat SDK Patcher/[Avatars] Build Actions/[Build] and [Upload]", priority = 3000)]
+        [MenuItem("VRChat SDK Patcher/[Avatars] Build Actions/[Build] and [Upload]", priority = 4000)]
         private static void BuildAndUpload()
         {
             AvatarBuildActionUtils.BuildAndUploadGuard();
         }
 
-        [MenuItem("VRChat SDK Patcher/[Avatars] Build Actions/[Upload] Last Build", priority = 3000)]
+        [MenuItem("VRChat SDK Patcher/[Avatars] Build Actions/[Upload] Last Build", priority = 4000)]
         private static void UploadLastBuild()
         {
             AvatarBuildActionUtils.UploadLastBuildGuard();
