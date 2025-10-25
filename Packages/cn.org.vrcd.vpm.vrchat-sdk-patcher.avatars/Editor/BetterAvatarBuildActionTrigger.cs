66ff14e2f54b69b33e880af56ffacdedf492fee2
Misaka-L <lipww1234@foxmail.com>
2025-10-25 14:30:28 +0800

    fix: compile error

--- Packages/cn.org.vrcd.vpm.vrchat-sdk-patcher.avatars/Editor/BetterAvatarBuildActionTrigger.cs
+++ Packages/cn.org.vrcd.vpm.vrchat-sdk-patcher.avatars/Editor/BetterAvatarBuildActionTrigger.cs
@@ -5,25 +5,25 @@ namespace VRCD.VRChatPackages.VRChatSDKPatcher.Avatars.Editor
 {
     internal static class BetterAvatarBuildActionTrigger
     {
-        [MenuItem(MenuItem("VRChat SDK Patcher (No longer supported)/[Avatars] Build Actions/[Build] Only", priority = 2000)]
+        [MenuItem(MenuItem("VRChat SDK Patcher (No longer supported)/[Avatars] Build Actions/[Build] Only", priority = 2000))]
         private static void Build()
         {
             AvatarBuildActionUtils.BuildGuard();
         }
 
-        [MenuItem(MenuItem("VRChat SDK Patcher (No longer supported)/[Avatars] Build Actions/[Build] and [Test]", priority = 3000)]
+        [MenuItem(MenuItem("VRChat SDK Patcher (No longer supported)/[Avatars] Build Actions/[Build] and [Test]", priority = 3000))]
         private static void BuildAndTest()
         {
             AvatarBuildActionUtils.BuildAndTestGuard();
         }
 
-        [MenuItem(MenuItem("VRChat SDK Patcher (No longer supported)/[Avatars] Build Actions/[Build] and [Upload]", priority = 4000)]
+        [MenuItem(MenuItem("VRChat SDK Patcher (No longer supported)/[Avatars] Build Actions/[Build] and [Upload]", priority = 4000))]
         private static void BuildAndUpload()
         {
             AvatarBuildActionUtils.BuildAndUploadGuard();
         }
 
-        [MenuItem(MenuItem("VRChat SDK Patcher (No longer supported)/[Avatars] Build Actions/[Upload] Last Build", priority = 4000)]
+        [MenuItem(MenuItem("VRChat SDK Patcher (No longer supported)/[Avatars] Build Actions/[Upload] Last Build", priority = 4000))]
         private static void UploadLastBuild()
         {
             AvatarBuildActionUtils.UploadLastBuildGuard();
